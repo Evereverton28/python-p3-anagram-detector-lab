78c99116491ff02ab61f2d80b58f05450e59a144
Everton <evertonkairu@gmail.com>
2024-05-28 11:41:22 +0300

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, base_word):
+        self.base_word = base_word.lower()
+        
+    def is_anagram(self, other_word):
+        return sorted(self.base_word) == sorted(other_word.lower())
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word) and word.lower() != self.base_word]
